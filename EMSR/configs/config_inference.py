0f15fd3c479a4595d60d9a3f80612c11de254252
Mohammad Khateri <157270655+mkhateri@users.noreply.github.com>
2026-01-07 05:57:12 +0200

    Delete EMSR directory

--- EMSR/configs/config_inference.py
+++
@@ -1,6 +0,0 @@
-INFERENCE_CONFIG = {
-    "patch_size": 341,          # your patch size
-    "stride": 170,              # your stride
-    "upscale_factor": 3,        # SR scaling
-    "mat_dataset_key": None     # or "volume" or "data" depending on your .mat file
-}
